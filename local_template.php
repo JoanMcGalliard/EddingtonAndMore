81e807eff0a2139840dabb40a869d3683c9f2fbc
Joan McGalliard <joan.mcgalliard@picsolve.com>
2016-01-09 14:19:08 +0000

    local_template corrected.

--- local_template.php
+++ local_template.php
@@ -1,9 +1,8 @@
 <?php
-// rename this to "local.php" and fill in values
 $timezone = "UTC";
 
 // device id sent to Endomondo.
-$DEVICE_ID=$_SERVER['HTTP_HOST'];
+$deviceId=$_SERVER['HTTP_HOST'];
 
 // Obtain from Strava, https://www.strava.com/settings/api
 $stravaClientId = null;
